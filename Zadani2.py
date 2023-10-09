b93056e0125042f45b3874101e46aeeb6ad849f1
Dominik Husák <domca.husak@gmail.com>
2023-10-09 10:27:48 +0200

    Third_Commit

--- Zadani2.py
+++ Zadani2.py
@@ -1,11 +1,21 @@
+import re
+
+
 def build_sql_select_all_from_table(table_name):
-    return "SELECT * FROM {}".format(table_name)
+    if not re.match(r'^[a-zA-Z_][a-zA-Z0-9_]*$', table_name):
+        raise ValueError("Neplatný název tabulky")
+    return f"SELECT * FROM {table_name}"
+
 
 def build_sql_select_custom_from_users(columns):
-    placeholders = ', '.join(columns)
-    sql = "SELECT {} FROM USER".format(placeholders)
-    return sql
+    if not all(re.match(r'^[a-zA-Z_][a-zA-Z0-9_]*$', column) for column in columns):
+        raise ValueError("Neplatný název sloupce")
+    columns_str = ", ".join(columns)
+    return f"SELECT {columns_str} FROM USER"
+
 
 def build_sql_select_users_order_by_custom(order_by_section):
-    return "SELECT * FROM USER ORDER BY {}".format(order_by_section)
+    if not re.match(r'^[a-zA-Z_][a-zA-Z0-9_]*(\s(ASC|DESC))?$', order_by_section):
+        raise ValueError("Neplatná sekce pro řazení")
+    return f"SELECT * FROM USER ORDER BY {order_by_section}"
 
